9baa7e77831216f3b7de1116f2cdbe2d033c1402
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-04-19 16:06:23 +0300

    IQ1

---
+++ src/day01/IQ1.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class IQ1 {
+    public static void main(String[] args) {
+        	/*
+ 		Type a program to print the numbers from 0 to 100 without using any integer
+ 		integer değişkeni kullanmadan, 0 da 100 e kadar sayıları bastıran bir program yazınız.
+    */
+
+
+        for (char c = (' '-32); c < 'e'; c++) {
+            System.out.println((int)c);
+
+        }
+    }
+}
